76cdaa9fdc65fbdfc6d4eae5e6613171077bba7b
Tommy Chu <tommy99chu@gmail.com>
2023-12-06 13:06:44 -0500

    added  dummy data, seatnumber is now in "uses"

--- schema.sql
+++ schema.sql
@@ -1,5 +1,3 @@
-drop database `cs336db`;
-
 CREATE DATABASE  IF NOT EXISTS `cs336db`;
 USE `cs336db`;
 
@@ -16,16 +14,21 @@ insert into aircraft (aircraftId, airlineId, numSeats) values ('dl1', 'DL', 3), 
 
 create table flight(flightNumber integer auto_increment, airline char(2), flownBy varchar(20) not null, departureAirport char(3) not null, departureTime timestamp not null, arrivalAirport char(3) not null, arrivalTime timestamp not null, price float not null,
 primary key(flightNumber, airline), foreign key(departureAirport) references airport(airportId), foreign key(arrivalAirport) references airport(airportId), foreign key(airline) references airline(airlineId), foreign key(flownBy) references aircraft(aircraftId));
-insert into flight (airline, flownBy, departureAirport, departureTime, arrivalAirport, arrivalTime, price) values ('UA', 'ua1', 'JFK', '2023-12-25 12:00:00', 'LAX', '2023-12-25 19:00:00', 300.00), ('UA', 'ua1', 'LAX', '2023-12-25 12:00:00', 'JFK', '2023-12-25 19:00:00', 300.00);
+insert into flight (airline, flownBy, departureAirport, departureTime, arrivalAirport, arrivalTime, price) values ('UA', 'ua1', 'JFK', '2023-12-25 12:00:00', 'LAX', '2023-12-25 19:00:00', 300.00);
+insert into flight (airline, flownBy, departureAirport, departureTime, arrivalAirport, arrivalTime, price) values ('UA', 'ua1', 'LAX', '2023-12-26 12:00:00', 'JFK', '2023-12-26 19:00:00', 300.00);
 
 create table account(username varchar(64) primary key, password varchar(64) not null, firstName varchar(50) not null, lastName varchar(50) not null, accountType varchar(20) not null);
 insert into account(username, password, firstName, lastName, accountType) values ('admin', 'admin', 'Jesus','Christ', 'admin');
+insert into account(username, password, firstName, lastName, accountType) values ('cust', 'cust', 'Test', 'Customer', 'customer');
 
-create table flightTicket(ticketID integer primary key auto_increment, totalFare float not null, class varchar(20) not null, changeFee float not null, bookingFee float not null, seatNumber varchar(3) not null, passenger varchar(64) not null, purchaseDateTime timestamp not null, 
+create table flightTicket(ticketID integer primary key auto_increment, totalFare float not null, class varchar(20) not null, changeFee float not null, bookingFee float not null, passenger varchar(64) not null, purchaseDateTime timestamp not null, 
 foreign key(passenger) references account(username));
+insert into flightTicket (totalFare, class, changeFee, bookingFee, passenger, purchaseDateTime) values (630, 'economy', 80, 30, 'cust', NOW());
 
-create table uses(flightNumber integer, ticketId integer,
+create table uses(flightNumber integer, ticketId integer, seatNumber varchar(3) not null, 
 primary key (flightNumber, ticketId), foreign key(flightNumber) references flight(flightNumber), foreign key(ticketID) references flightTicket(ticketId));
+insert into uses (flightNumber, ticketId, seatNumber) values (1, 1, 'A01');
+insert into uses (flightNumber, ticketId, seatNumber) values (2, 1, 'A12');
 
 create table inWaitingList(flightNumber integer, airlineId char(2), customer varchar(64), 
 primary key (flightNumber, airlineId, customer), foreign key(flightNumber) references flight(flightNumber), foreign key(airlineId) references airline(airlineId), foreign key(customer) references account(username));
